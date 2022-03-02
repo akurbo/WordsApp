e70db7f50dd88f57e9f7e404ba8f6827d855ae98
akurbo <artem-0072009@yandex.ru>
2022-03-02 19:03:14 +0300

    Initial Commit

---
+++ Words/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Words
+//
+//  Created by Kurbatov Artem on 02.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Words/WordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsApp.swift
+//  Words
+//
+//  Created by Kurbatov Artem on 02.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
